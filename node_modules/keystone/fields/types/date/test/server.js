9747f3cd58d55e9bbe95434552400beab277a961
Binayak Ghosh <ghoshbinayak@gmail.com>
2016-03-10 11:11:05 +0530

    use the latest keystone

--- node_modules/keystone/fields/types/date/test/server.js
+++ node_modules/keystone/fields/types/date/test/server.js
@@ -1,27 +1,43 @@
-var demand = require('must'),
-	DateType = require('../DateType');
+var demand = require('must');
+var DateType = require('../DateType');
 
-exports.initList = function(List) {
+exports.initList = function (List) {
 	List.add({
-		date: DateType
+		date: DateType,
 	});
 };
 
-exports.testFieldType = function(List) {
+exports.testFieldType = function (List) {
 	var testItem = new List.model();
 
-	it('should parse without error via underscore date', function() {
-		testItem._.date.parse('20131204', 'YYYYMMDD');
+	it('should parse date input and return a moment object', function () {
+		var m = List.fields.date.parse('2016-02-27');
+		demand(m.format('YYYY-MM-DD')).equal('2016-02-27');
 	});
-	
-	it('should be the date we expect', function() {
+
+	it('should format the date value using moment', function () {
 		testItem.date = new Date(2013, 11, 4);
-		demand(testItem._.date.format()).to.equal('4th Dec 2013');
-		demand(testItem._.date.format('YYYYMMDD')).to.equal('20131204');
+		demand(testItem._.date.format()).equal('4th Dec 2013');
+		demand(testItem._.date.format('YYYYMMDD')).equal('20131204');
 	});
-	
-	it('should be a moment object', function() {
+
+	it('should return a moment object set to the field value', function () {
 		testItem.date = new Date(2013, 11, 4);
 		demand(testItem._.date.moment()._isAMomentObject);
+		demand(testItem._.date.moment().format('YYYYMMDD')).equal('20131204');
+	});
+
+	it('should validate input present', function (done) {
+		List.fields.date.validateRequiredInput(this, { date: 'a' }, function (result) {
+			demand(result).be(true);
+			done();
+		});
+	});
+
+	it('should invalidate input not present', function (done) {
+		List.fields.date.validateRequiredInput(this, { date: '' }, function (result) {
+			demand(result).be(false);
+			done();
+		});
 	});
 };
